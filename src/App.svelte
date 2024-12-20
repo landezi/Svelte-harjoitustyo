3f2fa64370f8b4114b19e7c28534a74d4b6ef035
ae6130 <ae6130@student.jamk.fi>
2024-12-20 18:02:39 +0200

    Korttien haku

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script>
+  function getCards() {
+    fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US', {
+      headers: { Authorization: `Bearer ${import.meta.env.VITE_API_KEY}` },
+    });
+  }
 </script>
 
-<main></main>
+<main>
+  <button on:click={() => getCards()}>Hae kortteja</button>
+</main>
 
 <style>
+  button {
+    background-color: black;
+    color: burlywood;
+  }
 </style>
